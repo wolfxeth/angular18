2404dea658dcb9f841132ae947707d6933de32fb
Akshay kumar <33988336+wolfxeth@users.noreply.github.com>
2025-01-03 00:30:11 +0530

    code update

--- angular18/src/app/user-list/user-list.component.ts
+++ angular18/src/app/user-list/user-list.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { User, UserService } from '../user-service.service';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-user-list',
   imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
-  
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
   selectedUserId: string | null = null;
+  isLoading: boolean = true; // Set to true initially
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((data) => {
-      this.users = data;
-    });
+    this.userService.getUsers().subscribe(
+      (data: User[]) => {
+        this.users = data.sort((a, b) => a.name.localeCompare(b.name));
+        this.isLoading = false; // Set isLoading to false when data is fetched
+      },
+      (error: any) => {
+        console.error('Error fetching users:', error);
+        this.isLoading = false; // Handle error case by setting isLoading to false
+      }
+    );
   }
 
   onHover(userId: string): void {
