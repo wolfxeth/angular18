11fc8aa7775df7f2c7a091634fa982118b0bdf32
Akshay kumar <33988336+wolfxeth@users.noreply.github.com>
2025-01-06 01:44:05 +0530

    code update

--- angular18/src/app/app.routes.ts
+++ angular18/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './user-list/user-list.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'users', component: UserListComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }  // Default route
 ];
